69255322d87c22b1c87cf12ee01573305f69cc0e
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-09-19 14:22:38 -0300

    Criação da base do projeto

---
+++ calculadora_estudocaso_2309/bin/main.dart
@@ -0,0 +1,6 @@
+/*
+Crie uma classe Calculadora que execute as 4 operações matemáticas básicas via métodos que receberão dois parâmetros inteiros e deverão retornar o resultado da operação relativa.
+Crie um método display, que deverá mostrar na saída padrão a operação feita com os dois argumentos, além do resultado.
+A cada operação, o resultado deverá ser mostrado chamando-se o método display.
+Crie também, uma classe executora, instancie um objeto da classe Calculadora, chame os 4 métodos criados com as operações matemáticas, passando 2 valores inteiros em cada um.
+*/
